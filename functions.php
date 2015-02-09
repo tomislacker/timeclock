139f2851f9173e3fdaae544fea5f906ac0b5b6c6
Ben Tomasik <btomasik@telkonet.com>
2015-02-08 20:24:49 -0600

    Suppress notices for undefined variables

--- functions.php
+++ functions.php
@@ -290,13 +290,13 @@ function ip_range($network, $ip) {
 }
 
 function setTimeZone() {
-    if ($use_client_tz == "yes") {
+    if (@$use_client_tz == "yes") {
         if (isset($_COOKIE['tzoffset'])) {
             $tzo = $_COOKIE['tzoffset'];
             settype($tzo, "integer");
             $tzo = $tzo * 60;
         }
-    } elseif ($use_server_tz == "yes") {
+    } elseif (@$use_server_tz == "yes") {
         $tzo = date('Z');
     } else {
         $tzo = 0;
